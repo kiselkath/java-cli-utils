c6f76b27ebda5bdc5751eece7afb14361530d127
katekiselevich <kiselkath@gmail.com>
2025-04-11 09:50:42 +0200

    Initial commit with HelloUtils.java

--- HelloUtils.java
+++ HelloUtils.java
@@ -1,11 +1,7 @@
-//package utils;
-//
-//public class HelloUtils {
-//    public static Stringx sayHello(String name) {
-//        return "Hello, " + name + "!";
-//    }
-//}
+package utils;
 
-public static String greetTimeOfDay(String name, String time) {
-    return "Good " + time + ", " + name + "!";
+public class HelloUtils {
+    public static Stringx sayHello(String name) {
+        return "Hello, " + name + "!";
+    }
 }
