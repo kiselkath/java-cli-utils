dac19dbbe0a6a622d6a7284355ba8242fb710c3c
katekiselevich <kiselkath@gmail.com>
2025-04-11 09:53:45 +0200

    Delted method from HelloUtils.java

--- HelloUtils.java
+++ HelloUtils.java
@@ -1,11 +1,3 @@
-//package utils;
-//
-//public class HelloUtils {
-//    public static Stringx sayHello(String name) {
-//        return "Hello, " + name + "!";
-//    }
-//}
-
 public static String greetTimeOfDay(String name, String time) {
     return "Good " + time + ", " + name + "!";
 }
